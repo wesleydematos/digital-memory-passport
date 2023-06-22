ee41cc160883567d3e42517b44d0890b4d3927dc
wesleydematos <wesleydematos3@gmail.com>
2023-06-22 15:48:12 -0300

    fix: adjust backgrounds

--- src/components/Backgrounds/style.js
+++ src/components/Backgrounds/style.js
@@ -1,31 +1,18 @@
 import styled from "styled-components";
 
-export const BgHome = styled.div`
-  background-color: var(--blue-l);
-  height: 100vh;
-`;
-
-export const BgPage2 = styled.div`
+const BgAllOrange = styled.div`
   background-color: var(--orange-l);
   height: 100vh;
 `;
 
-export const BgPage3 = styled.div`
+const BgOrangeBlue = styled.div`
   background-color: var(--orange-l);
   height: 100vh;
 `;
 
-export const BgPage4 = styled.div`
+const BgBlueOrange = styled.div`
   background-color: var(--blue-l);
   height: 100vh;
 `;
 
-export const BgPage5 = styled.div`
-  background-color: var(--blue-l);
-  height: 100vh;
-`;
-
-export const BgPage6 = styled.div`
-  background-color: var(--blue-l);
-  height: 100vh;
-`;
+export { BgAllOrange, BgBlueOrange, BgOrangeBlue };

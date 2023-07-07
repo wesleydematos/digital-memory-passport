efad5b38d758b34e6817aef44baf709807ce3cba
wesleydematos <wesleydematos3@gmail.com>
2023-07-07 12:56:46 -0300

    fix: update city page

--- src/pages/City/index.jsx
+++ src/pages/City/index.jsx
@@ -1,5 +1,7 @@
-import { SquarePurple } from "../Square/style";
+import { SquarePurple } from "../../components/Square/style";
 import { CityContent } from "./style";
+import sun from "../../assets/sol.png";
+import cloud from "../../assets/nuvem.png";
 
 function City() {
   const city = "Syros";
@@ -16,10 +18,12 @@ function City() {
             {city.toUpperCase()} <br /> <span>{country.toUpperCase()}</span>
           </h1>
           <p className="fact">{fact}</p>
-          <p className="scan">SCAN THE QR CODE TO GET YOURS!</p>
+          <button className="scan">SCAN THE QR CODE TO GET YOURS!</button>
         </div>
         <div className="code">
+          <img src={sun} alt="Sol" id="sun" />
           <SquarePurple />
+          <img src={cloud} alt="nuvem" id="cloud" />
         </div>
       </main>
     </CityContent>
---
+++ src/pages/City/style.js
@@ -0,0 +1,171 @@
+import styled from "styled-components";
+
+export const CityContent = styled.div`
+  background-color: var(--orange-l);
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  overflow: hidden;
+  color: var(--white-l);
+
+  > p {
+    font-family: "Barrio", sans-serif;
+    font-size: 2rem;
+    margin-bottom: 2vh;
+  }
+
+  main {
+    width: 100vw;
+    height: calc(93vh);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    width: 90%;
+    gap: 5vh;
+
+    h1 {
+      font-size: 2rem;
+      text-align: center;
+      line-height: 0.6;
+      font-weight: 900;
+    }
+
+    h1 > span {
+      color: var(--blue-l);
+      font-size: 2.2rem;
+    }
+
+    button {
+      background-color: #d9bb8d;
+      color: var(--white-l);
+    }
+  }
+
+  .code {
+    height: 250px;
+    width: 250px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    overflow: hidden;
+
+    #sun,
+    #cloud {
+      height: 120px;
+      width: 120px;
+    }
+
+    #sun {
+      z-index: 10;
+      align-self: flex-start;
+    }
+
+    div {
+      position: absolute;
+      align-self: center;
+      margin-left: 45px;
+      z-index: 100;
+    }
+
+    #cloud {
+      z-index: 110;
+      align-self: flex-end;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .code {
+      height: 340px;
+      width: 340px;
+    }
+
+    #cloud,
+    #sun {
+      height: 210px;
+      width: 210px;
+    }
+
+    #cloud {
+      margin-left: 220px;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    > p {
+      font-size: 4.5rem;
+    }
+
+    main {
+      flex-direction: row;
+      align-items: flex-start;
+      justify-content: flex-start;
+    }
+
+    .content,
+    .code {
+      width: 50vw;
+    }
+
+    .code {
+      height: 390px;
+      width: 390px;
+    }
+
+    #cloud,
+    #sun {
+      height: 260px;
+      width: 260px;
+    }
+
+    #cloud {
+      margin-left: 270px;
+    }
+
+    .content {
+      padding-left: 5vw;
+      align-items: center;
+      gap: 15vh;
+
+      h1,
+      h1 > span {
+        font-size: 6rem;
+      }
+    }
+
+    .fact {
+      width: 59%;
+      font-size: 2rem;
+      text-align: left;
+    }
+
+    button {
+      width: 80%;
+      font-size: 1.8rem;
+    }
+  }
+
+  @media (min-width: 2400px) {
+    .code {
+      height: 490px;
+      width: 490px;
+    }
+
+    #cloud,
+    #sun {
+      height: 360px;
+      width: 360px;
+    }
+
+    #cloud {
+      margin-left: 370px;
+    }
+  }
+`;
